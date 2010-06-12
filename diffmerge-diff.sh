cc032e796c18b2f8fad421bdbd2359b84080e099
Borje Granberg <borje.granberg@gmail.com>
2010-06-12 18:18:17 +0200

    Snyggat upp och förhoppningsvis förbättrat diffmerge-diff.sh och anropet

--- diffmerge-diff.sh
+++ diffmerge-diff.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
- 
-path="$(cygpath $1)"
-old="$(cygpath --mixed --absolute "$2")"
-new="$(cygpath --mixed --absolute "$5")"
- 
+
+path="$(cygpath $2)"
+old="$(cygpath --mixed --absolute "$1")"
+new="$(cygpath --mixed --absolute "$2")"
+
 #echo -e "path\n$path"
 #echo -e "old\n$old"
 #echo -e "new\n$new"
