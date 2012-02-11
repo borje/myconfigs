e0df9c6067359d76941c226bf19f3da4945dbaf5
Borje Granberg <borje.granberg@gmail.com>
2012-02-11 12:25:38 +0100

    Changed border coler of the window in focus

--- rc.lua
+++ rc.lua
@@ -351,6 +351,6 @@ client.add_signal("manage", function (c, startup)
     end
 end)
 
-client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+client.add_signal("focus", function(c) c.border_color = "#00FF00" end)
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
