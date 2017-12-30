91a84407e9beda7fb6b5100cbd3966f90caf07c6
Borje Granberg <borje.granberg@gmail.com>
2017-12-30 22:16:11 +0100

    added fzf to vim and bash

---
+++ bash/fzf.sh
@@ -0,0 +1,21 @@
+# fd - cd to selected directory
+fd() {
+  local dir
+  dir=$(find ${1:-.} -path '*/\.*' -prune \
+                  -o -type d -print 2> /dev/null | fzf +m) &&
+  cd "$dir"
+}
+
+# Install (one or multiple) selected application(s)
+# using "brew search" as source input
+# mnemonic [B]rew [I]nstall [P]lugin
+bip() {
+  local inst=$(brew search | fzf -m)
+
+  if [[ $inst ]]; then
+    for prog in $(echo $inst)
+    do 
+        brew install $prog
+    done
+  fi
+}
